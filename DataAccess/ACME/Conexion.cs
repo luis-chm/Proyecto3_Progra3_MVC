db793390f8a3e7745dae198b4c267d04d08e422d
luis-chm <luischavesmora@gmail.com>
2024-04-15 03:15:58 -0600

    Add project files.

---
+++ DataAccess/ACME/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DataAccess.ACME
+{
+    public class Conexion
+    {
+        public readonly string? _cadenaConexion;
+
+        public Conexion()
+        {
+            string? cadenaConexion;
+            //obtiene la cadena de conexion de la variable añadida manualmente
+            cadenaConexion = Environment.GetEnvironmentVariable("SQLServeEX-MVC");
+            _cadenaConexion = cadenaConexion;
+        }
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection sqlConn;
+            sqlConn = new SqlConnection(_cadenaConexion);//instancia
+            return sqlConn;
+        }
+    }
+}
---
+++ Services/ACME/EmpresaService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Models.ACME;
+using DataAccess.ACME;
+
+namespace Services.ACME
+{
+    public class EmpresaService
+    {
+        public bool crear(EmpresaEntidad empresaEntidad)
+        {
+            EmpresaDA empresaDA = new EmpresaDA();
+
+            try
+            {
+                empresaDA.Insertar(empresaEntidad);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool Actualizar(EmpresaEntidad empresaEntidad)
+        {
+            EmpresaDA empresaDA = new EmpresaDA();
+
+            try
+            {
+                empresaDA.Modificar(empresaEntidad);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public EmpresaEntidad? BuscarxID(int IDEmpresa)
+        {
+            EmpresaDA? empresaDA = new EmpresaDA();
+
+            try
+            {
+                return empresaDA.BuscarID(IDEmpresa);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public List<EmpresaEntidad>? Listar()
+        {
+            EmpresaDA? empresaDA = new EmpresaDA();
+
+            try
+            {
+                return empresaDA.Listar();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public bool Eliminar(EmpresaEntidad empresaEntidad)
+        {
+            EmpresaDA empresaDA = new EmpresaDA();
+
+            try
+            {
+                empresaDA.Anular(empresaEntidad);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
